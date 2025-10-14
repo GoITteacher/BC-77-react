9296c1dcffb62ef41cb18445d40954c48d347e9f
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-10-14 08:28:15 +0300

    before

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,21 +1,9 @@
-import { useCreateTask } from "../../hooks/useCreateTask";
 import css from "./TaskForm.module.css";
 
-interface TaskFormProps {
-  onSuccess: () => void;
-}
-
-export default function TaskForm({ onSuccess }: TaskFormProps) {
-  const { mutate, isPending } = useCreateTask(onSuccess);
-
-  const handleSubmit = (formData: FormData) => {
-    mutate({
-      text: formData.get("text") as string,
-    });
-  };
-
+export default function TaskForm() {
+  const isPending = false;
   return (
-    <form className={css.form} action={handleSubmit}>
+    <form className={css.form}>
       <label className={css.label}>
         Task text
         <textarea name="text" className={css.input} rows={5}></textarea>
