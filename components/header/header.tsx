ac7c9b95bc546b53f2137afb61b1eb3279b1e6e0
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2025-10-20 11:52:21 +0300

    update

---
+++ components/header/header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import css from "./header.module.css";
+
+const Header = () => {
+  return (
+    <header className={css["header"]}>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/profile">Profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
